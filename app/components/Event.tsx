69d77488a6afe33256600c1c2dcff8dde16c658a
AdamMalm <adam.hornemalm@gmail.com>
2022-05-18 12:03:10 +0200

    pull req

--- app/components/Event.tsx
+++ app/components/Event.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
-import { Card } from 'react-native-elements'
+import { StyleSheet, Text, Image, View } from 'react-native'
 
-const Event = () => {
+const Event = ({ title, description, location, eventImage, noGoing, imageGoing1, imageGoing2, imageGoing3 }: 
+               { title: string; description: string;  location: string; eventImage: string; noGoing: number; 
+                 imageGoing1: string; imageGoing2: string; imageGoing3: string}) => {
+  
   return (
-    <Card containerStyle={styles.container}>
-        <Text>Event</Text>
-    </Card>
+    <View style={styles.container}>
+        {// <Image source={require(eventImage)}/>
+        }
+        <Text>{title}</Text>
+        <Text>{description}</Text>
+        <Text>{location}</Text>
+        <Text>+{noGoing} Going</Text>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
       backgroundColor: 'red',
+      maxWidth: "40%",
+      display: 'flex',
+      justifyContent: 'center',
+      
     },
   });
 
--- app/screens/Eventpage.tsx
+++ app/screens/Eventpage.tsx
@@ -5,8 +5,17 @@ import Event from '../components/Event'
 const Eventpage = () => {
   return (
     <ScrollView>
-        <Text>Event Page</Text>
-        <Event />
+        <Event 
+          title="Example Title" 
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+          location="Ducklake"
+          eventImage="../assets/Duckfeeding.jpg"
+          noGoing={10}
+          imageGoing1="../assets/going1.jpg"
+          imageGoing2="../assets/going2.jpg"
+          imageGoing3="../assets/going3.jpg"
+        />
+    
     </ScrollView>
   )
 }
